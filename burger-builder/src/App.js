1cd5542e8a56efb1314d19caca10f944bd862a20
Caroline Salib <carolinesalibc@gmail.com>
2019-04-17 07:54:16 -0600

    Create Layout component

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Layout from '../src/components/Layout/Layout'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Test
-      </div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
     );
   }
 }
