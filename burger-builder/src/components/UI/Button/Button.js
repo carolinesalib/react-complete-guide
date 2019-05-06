02985b45177babea2fcfbcf4d6695fafb825cc46
Caroline Salib <carolinesalibc@gmail.com>
2019-05-06 07:32:43 -0600

    Add cancel and continue button for order summary

--- burger-builder/src/components/UI/Button/Button.js
+++ burger-builder/src/components/UI/Button/Button.js
@@ -2,8 +2,8 @@ import React from 'react';
 import classes from './Button.css';
 
 const button = (props) => {
-    <button
-        className{[classes.Button, classes[props.btnType]].join(' ')}
+    return <button
+        className={[classes.Button, classes[props.btnType]].join(' ')}
         onClick={props.clicked}
     >
         {props.children}
