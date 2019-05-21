d795f27e1e3389c44d979c2a630405c71932f743
Caroline Salib <carolinesalibc@gmail.com>
2019-05-21 07:22:10 -0600

    Update state immutably
    - 290

--- redux--01-start/src/containers/Counter/Counter.js
+++ redux--01-start/src/containers/Counter/Counter.js
@@ -13,6 +13,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 8" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 8" clicked={this.props.onSubtractCounter}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -20,7 +27,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results,
     };
 };
 
@@ -30,6 +38,8 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
         onAddCounter: () => dispatch({type: 'ADD', value: 8}),
         onSubtractCounter: () => dispatch({type: 'SUBTRACT', value: 8}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
     };
 };
 
