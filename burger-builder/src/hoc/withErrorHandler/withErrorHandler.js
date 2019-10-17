27b6556349c42fc1d86f7c472babe7888e1bfe95
Caroline Salib <carolinesalibc@gmail.com>
2019-10-16 20:04:10 -0600

    Removing Old Interceptors
    [213]

--- burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -11,14 +11,19 @@ const withErrorHandler = (WrappedComponent, axios) => {
         constructor(props) {
             super(props);
 
-            axios.interceptors.request.use(request => {
+            this.requestInterceptor = axios.interceptors.request.use(request => {
                 this.setState({error: null});
                 return request;
             });
 
-            axios.interceptors.response.use(res => res, error => {
+            this.responseInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error: error});
             })
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.requestInterceptor);
+            axios.interceptors.response.eject(this.responseInterceptor);
         }
 
         errorConfirmedHandler = () => {
