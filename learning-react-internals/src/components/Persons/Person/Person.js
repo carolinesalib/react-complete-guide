8168b6af2996fb42819e62c8a354f57e466b45fa
Caroline Salib <carolinesalibc@gmail.com>
2021-01-04 08:27:11 -0700

    Using PropTypes
    109

--- learning-react-internals/src/components/Persons/Person/Person.js
+++ learning-react-internals/src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 // we don't need the component because we aren't use Component class, just declaring a function
 import React from 'react';
 import classes from './Person.css';
+import PropTypes from 'prop-types';
 
 // const because we don't intent to change this variable
 const person = (props) => {
@@ -11,5 +12,12 @@ const person = (props) => {
       <input type="text" onChange={props.changed} value={props.name} />
     </div>);
 }
+
+person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func,
+};
 
 export default person;
