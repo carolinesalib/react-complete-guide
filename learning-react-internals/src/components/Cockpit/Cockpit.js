229ddf6197d7423d2f139f3c22d3699c5bf6ab1a
Caroline Salib <carolinesalibc@gmail.com>
2021-01-04 08:52:58 -0700

    Refs with React Hooks
    110, 111

--- learning-react-internals/src/components/Cockpit/Cockpit.js
+++ learning-react-internals/src/components/Cockpit/Cockpit.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+    const toggleButtonRef = useRef(null);
     const assignedClasses = [];
     let btnClass = '';
+
+    useEffect(() => {
+        toggleButtonRef.current.click();
+    }, []);
 
     if (props.showPersons) {
         btnClass = classes.Red;
@@ -12,18 +17,21 @@ const cockpit = (props) => {
     if (props.persons.length <= 2) {
         assignedClasses.push(classes.red);
     }
+
     if (props.persons.length <= 1) {
         assignedClasses.push(classes.bold);
     }
-
 
     return (
         <div className={classes.Cockpit}>
             <h1>Hi, I'm a react app</h1>
             <p className={assignedClasses.join(' ')}>This is a real ...</p>
             <button
-            className={btnClass}
-            onClick={props.clicked}>Switch Name</button>
+                ref={toggleButtonRef}
+                className={btnClass}
+                onClick={props.clicked}
+            >Switch Name
+            </button>
         </div>
     );
 };
--- learning-react-internals/src/components/Persons/Person/Person.js
+++ learning-react-internals/src/components/Persons/Person/Person.js
@@ -1,15 +1,26 @@
 // we don't need the component because we aren't use Component class, just declaring a function
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import classes from './Person.css';
 import PropTypes from 'prop-types';
 
 // const because we don't intent to change this variable
 const person = (props) => {
+  const inputElementRef = useRef(null);
+
+  useEffect(() => {
+    inputElementRef.current.focus();
+  }, []);
+
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old!</p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
+      <input
+        type="text"
+        ref={inputElementRef}
+        onChange={props.changed}
+        value={props.name}
+      />
     </div>);
 }
 
