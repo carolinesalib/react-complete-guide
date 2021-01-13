bb438ce443a2ec0a8675088bcd1a21e670f4aef6
Caroline Salib <carolinesalibc@gmail.com>
2021-01-13 07:50:31 -0700

    Add Order Actions
    308

--- burger-builder/src/store/actions/order.js
+++ burger-builder/src/store/actions/order.js
@@ -1,2 +1,30 @@
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
-export const REMOVE_INGREDIENT = 'REMOVE_INGREDIENT';
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData: orderData,
+    };
+};
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_FAIL,
+        error: error,
+    };
+};
+
+export const purchaseBurgerStart = (orderData) => {
+    return dispatch => {
+        axios.post('/orders.json', orderData)
+            .then(response => {
+                console.log(response.data, orderData)
+                dispatch(purchaseBurgerSuccess(response.data))
+            })
+            .catch(error => {
+                dispatch(purchaseBurgerFail(error));
+            });
+    };
+};
