73195d1c0b09e5289391be3865d6430ac1c4c683
Caroline Salib <carolinesalibc@gmail.com>
2021-03-13 10:19:42 -0700

    Update auth url

--- redux-saga--01-start/src/store/actions/auth.js
+++ redux-saga--01-start/src/store/actions/auth.js
@@ -48,10 +48,13 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+
+        let url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyArwjaHjD49s6V3eD1ZnjqyLcleb6BZjZQ";
+
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
+            url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyArwjaHjD49s6V3eD1ZnjqyLcleb6BZjZQ";
         }
+
         axios.post(url, authData)
             .then(response => {
                 const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
