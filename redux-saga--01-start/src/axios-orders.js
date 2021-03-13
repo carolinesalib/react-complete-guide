e7ff81f2200f6033a50c7d8336fe506e64d99267
Caroline Salib <carolinesalibc@gmail.com>
2021-03-13 16:42:23 -0700

    Move order side effects into sagas
    421

--- redux-saga--01-start/src/axios-orders.js
+++ redux-saga--01-start/src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-burger.firebaseio.com/'
+    baseURL: 'https://react-by-burger-ab41a.firebaseio.com/'
 });
 
 export default instance;
